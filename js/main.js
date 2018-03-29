c58f4c3b3968190209b9d316017a5a7e493b455b
Natalie Soberano <nataliesoberano@natalies-mbp-2.fc.ca>
2018-03-29 10:07:47 -0400

    in class changes with controls

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 (() => {
 	console.log('video stuff loaded!');
+
+	String.prototype.capIt = function() { return this.replace(this.charAt(), this.charAt().toUpperCase()); }
 	
 	//variable stack goes here
 	let silgils = document.querySelectorAll('.sigilContainer'), 
 		lightbox = document.querySelector('.lightbox'),
 		closeLightboxButton = lightbox.querySelector('.close-lightbox'),
-		vidPlayer = document.querySelector('video');
+		vidPlayer = document.querySelector('video')
+		vidControls = document.querySelector('.controls');
 
 	// functions in the middle 
 
 	function showHouseVideo () {
+		let houseName = this.className.split(' ')[1].capIt();
+		//split apart the class name on the element, grabe the house from the result
+		document.querySelector('h1').textContent = `House ${houseName}`;
 		//debugger
 		lightbox.classList.add('show-lightbox');
 		//make it play 
+		vidPlayer.src =
+		`video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
+		vidPlayer.load();
 		vidPlayer.play();
+
+	}
+
+	function pausePlay () {
+		//debugger;
+		let theButton = this.firstElementChild;
+		//
+		if (vidPlayer.paused == true) {
+			// play the video
+			vidPlayer.play();
+			theButton.dataset.icon = "pause-circle";
+		} else {
+			vidPlayer.pause();
+			theButton.dataset.icon = "play-circle";
+		}
 	}
 
 	function closeLightbox () {
@@ -21,15 +45,15 @@
 		lightbox.classList.remove('show-lightbox');
 		//stop the video and rewind it to 0
 		vidPlayer.pause();
-		vidPlayer.currentTime =0;
-
+		vidPlayer.currentTime = 0;
 	}
-
 
 	// event handeling at the bottom 
 
 	silgils.forEach(silgil => silgil.addEventListener('click', showHouseVideo));
 	closeLightboxButton.addEventListener('click', closeLightbox);
+	vidPlayer.addEventListener('ended', closeLightbox);
+	vidControls.addEventListener('click', pausePlay);
 
 
 
